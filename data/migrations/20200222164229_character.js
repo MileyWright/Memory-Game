72c6a9a5942dd4751cf09d59b627276231e7ef4a
Miley Wright <dishaliekwright@yahoo.com>
2020-02-22 17:59:13 -0500

    migrations created for all tables

--- data/migrations/20200222164229_character.js
+++ data/migrations/20200222164229_character.js
@@ -1,22 +1,70 @@
 
 exports.up = function(knex) {
     return knex.schema.createTable('Cartoon', cartoons => {
-        
+       cartoons.increments()
+       cartoons.string('name', 126)
+       .notNullable()
+       .unique();
+       cartoons.string('show', 126)
+       .notNullable();
+       cartoons.string('image', 255)
+       .notNullable()
+       .unique();
+       cartoons.boolean('clicked')
+       .notNullable();
     }),
     knex.schema.createTable('Anime', anime => {
-
+        anime.increments()
+        anime.string('name', 126)
+        .notNullable()
+        .unique();
+        anime.string('show', 126)
+        .notNullable();
+        anime.string('image', 255)
+        .notNullable()
+        .unique();
+        anime.boolean('clicked')
+        .notNullable();
     }),
     knex.schema.createTable('Avengers', avengers => {
-    
+        avengers.increments()
+        avengers.string('name', 126)
+        .notNullable()
+        .unique();
+        avengers.string('image', 255)
+        .notNullable()
+        .unique();
+        avengers.boolean('clicked')
+        .notNullable();
     }),
     knex.schema.createTable('FamilyGuy', familyguy => {
-
+        familyguy.increments()
+        familyguy.string('name', 126)
+        .notNullable()
+        .unique();
+        familyguy.string('image', 255)
+        .notNullable()
+        .unique();
+        familyguy.boolean('clicked')
+        .notNullable();
     }),
     knex.schema.createTable('RickMorty', rickmorty => {
-
+        rickmorty.increments()
+        rickmorty.string('name', 126)
+        .notNullable()
+        .unique();
+        rickmorty.string('image', 255)
+        .notNullable()
+        .unique();
+        rickmorty.boolean('clicked')
+        .notNullable();
     })
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema.dropTableIfExists('RickMorty'),
+  knex.schema.dropTableIfExists('FamilyGuy'),
+  knex.schema.dropTableIfExists('Avengers'),
+  knex.schema.dropTableIfExists('Anime'),
+  knex.schema.dropTableIfExists('Cartoon')
 };
