c667a82094cc12c75df9a9c9c176c8749290679a
Miley Wright <dishaliekwright@yahoo.com>
2020-02-22 23:17:16 -0500

    GET request to /90sCartoon

--- Routes/90sCartoon/cartoonRouter.js
+++ Routes/90sCartoon/cartoonRouter.js
@@ -3,5 +3,15 @@ const cartoon = require('./cartoon-model');
 const router = express.Router();
 
 // GET /90sCartoon character
+router.get('/', (req,res) => {
+    cartoon.get()
+    .then(cartoon => {
+        res.status(200).json(cartoon)
+    })
+    .catch(err => {
+        console.log(err)
+        res.status(500).json(err)
+    })
+})
 
 module.exports = router;
