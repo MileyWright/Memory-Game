544a41a20eda3f5fe1ba4ed9a3b7f2be8c1cae5a
Miley Wright <dishaliekwright@yahoo.com>
2020-02-24 14:24:44 -0500

    routes set up

--- solidmemory/src/App.js
+++ solidmemory/src/App.js
@@ -3,7 +3,11 @@ import './App.css';
 import MainPage from './components/MainPage';
 import {Route} from 'react-router-dom';
 import Cartoon from './components/Categories/Cartoon';
-import 
+import Anime from './components/Categories/Anime';
+import Avengers from './components/Categories/Avengers';
+import FamilyGuy from './components/Categories/FamilyGuy';
+import RicknMorty from './components/Categories/RicknMorty';
+
 function App() {
 
   
@@ -15,6 +19,11 @@ function App() {
       <header className="App-header">
         <Route exact path='/' component={MainPage}/>
         <Route exact path='/90scartoon' component={Cartoon}/>
+        <Route exact path='/anime' component={Anime}/>
+        <Route exact path='/avengers' component={Avengers}/>
+        <Route exact path='/familyguy' component={FamilyGuy}/>
+        <Route exact path='/ricknmorty' component={RicknMorty}/>
+
       </header>
 
     </div>
