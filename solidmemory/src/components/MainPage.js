316e0186e62bbfc55bfbb24a63151749208356d0
Miley Wright <dishaliekwright@yahoo.com>
2020-02-24 14:46:23 -0500

    buttons are up and linked

--- solidmemory/src/components/MainPage.js
+++ solidmemory/src/components/MainPage.js
@@ -1,10 +1,25 @@
 import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
 
 class MainPage extends Component {
     render(){
         return(
             <>
-            MainPage here
+            <Link path to='/90scartoon'>
+                <button>90s Cartoons</button>
+            </Link>
+            <Link path to='/anime'>
+                <button>Anime</button>
+            </Link>
+            <Link path to='/avengers'>
+                <button>Advengers</button>
+            </Link>
+            <Link path to='/familyguy'>
+                <button>Family Guy</button>
+            </Link>
+            <Link path to='/ricknmorty'>
+                <button>Rick n Morty</button>
+            </Link>
             </>
         )
     }
