f32e026acf2fb6525fc9dc78e32dfd5f07eb6b96
Miley Wright <dishaliekwright@yahoo.com>
2020-02-24 13:36:10 -0500

    structure set up

---
+++ solidmemory/src/components/Cartoon.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Cartoon extends Component {
+    render(){
+        return(
+            <>
+            Cartoon here
+            </>
+        )
+    }
+}
+
+export default Cartoon;
---
+++ solidmemory/src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Navbar extends Component {
+    render(){
+        return(
+            <>
+            Navbar here
+            </>
+        )
+    }
+}
+
+export default Navbar;
