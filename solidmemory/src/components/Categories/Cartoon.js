9634e366fcb04aff7bde4d27bf3dfdb4cc4a6733
Miley Wright <dishaliekwright@yahoo.com>
2020-02-25 01:29:16 -0500

    created shuffle, increment, ans score handles

--- solidmemory/src/components/Categories/Cartoon.js
+++ solidmemory/src/components/Categories/Cartoon.js
@@ -20,6 +20,65 @@ class Cartoon extends Component {
         console.log(res.data)
         })
     }
+
+    handleClick = id => {
+        this.shuffleArray();
+        this.handleScore(id);
+        console.log(this.state.timesClicked);
+    }
+
+    handleScore = id => {
+        this.state.cartoon.forEach(item => {
+            if (id === item.id && item.clicked === false) {
+                item.clicked = true;
+                this.setState({Clicked: false});
+                this.handleIncrement();
+            } else if ( id === item.id && item.clicked === true) {
+                if (this.state.currentScore > this.state.highScore) {
+                    this.setState({highScore: this.state.currentScore});
+                }
+                this.setState({currentScore: 0});
+                this.setState({Clicked: true});
+                this.state.cartoon.forEach(item =>
+                    (item.clicked = false)
+                )
+            }
+        });
+    };
+
+    shuffleArray = () => {
+        //shuffles the array of objects
+        const shuffledArr = this.shuffle(this.state.cartoon);
+        this.setState({shuffledArr});
+    };
+
+    handleIncrement = () => {
+        this.setState({currentScore: this.state.currentScore + 1})
+    };
+
+    //function that takes an array as a parameter and shuffles it
+    shuffle = array => {
+        let currentIndex = array.length,
+        temporaryValue,
+        randomIndex;
+
+        //while there remain elements to shuffle
+        while (0 !== currentIndex) {
+            //pick a remaining item
+            randomIndex = Math.floor(Math.random() * currentIndex);
+            currentIndex -= 1;
+        
+
+        //then swap it with the current item
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+        }
+        return array
+    };
+
+
+
     render(){
         return(
             <div>
